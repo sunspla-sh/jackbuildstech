074a7f1d86c1df5d0566f1061bc90a24e0662e41
jack <jrfisch95@gmail.com>
2023-05-02 18:36:33 -0500

    finish tech skills section

--- modal.js
+++ modal.js
@@ -1,39 +1,41 @@
-
 export const createModal = event => {
 
   const techIcon = event.target
-  console.log(event.target)
   
   const modalBackground = document.createElement('div');
   modalBackground.classList.add('modal-background');
-  modalBackground.style.width = `${document.body.clientWidth}px`;
-  modalBackground.style.height = `${document.body.clientHeight}px`;
-
 
   const modalContainer = document.createElement('div');
   modalContainer.classList.add('modal-container');
-  modalContainer.style.top = `calc(${window.scrollY}px + 10vh)`;
 
   modalContainer.innerHTML = `
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+    </svg>
     <img src=${techIcon.src}>
-    <h3>${techIcon.dataset.name}</h3>
-    <p lang="en">${techIcon.dataset.description}</p>
+    <h3>${techIcon.title}</h3>
+    <p>${techIcon.dataset.description}</p>
     <a href="${techIcon.dataset.url}" target="_blank">Learn More</a>
   `;
 
   document.body.appendChild(modalBackground);
-  document.body.appendChild(modalContainer); 
+  document.body.appendChild(modalContainer)
 
   modalBackground.addEventListener('click', () => {
     modalBackground.remove();
     modalContainer.remove();
   });
 
+  const closeButton = document.querySelector('.modal-container svg');
+
+  closeButton.addEventListener('click', () => {
+    modalBackground.remove();
+    modalContainer.remove();
+  });
+
 }
 
-export const resizeModalBackground = event => {
 
-};
 
 export const scrollModalContainer = event => {
 
