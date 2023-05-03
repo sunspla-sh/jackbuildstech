7426c894c50590d90ffb3ce6d3855ce02fe19dfd
jack <jrfisch95@gmail.com>
2023-05-03 09:37:32 -0500

    update projects section, tech section, experience section

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 import { createModal } from "./modal";
 
 const techIcons = document.querySelector('.tech-icons-container').children;
+const projectCardTechIconsContainers = document.querySelectorAll('.project-card-tech-icons-container');
 
 for(let i = 0; i < techIcons.length; i++){
 
   techIcons[i].addEventListener('click', createModal);
 
 }
+
+for(let i = 0; i < projectCardTechIconsContainers.length; i++){
+
+  const projectCardTechIcons = projectCardTechIconsContainers[i].children;
+
+  for(let j = 0; j < projectCardTechIcons.length; j++) {
+    projectCardTechIcons[j].addEventListener('click', createModal);
+  } 
+
+}
+
